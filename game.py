02284c149f4907e6d251ce2168458c241b63d21b
Adelina2302 <adelina2302.dunina@gmail.com>
2025-03-23 20:59:42 -0400

    Первый коммит

---
+++ game.py
@@ -0,0 +1,76 @@
+import os
+from faker import Faker
+from my_file_operations import render_template
+import random
+
+
+folder_path = "Cards"
+
+
+fake = Faker("ru_RU")
+
+skills = [
+    "Стремительный прыжок", "Электрический выстрел", "Ледяной удар",
+    "Стремительный удар", "Кислотный взгляд", "Тайный побег",
+    "Ледяной выстрел", "Огненный заряд"
+]
+
+letters = {
+    'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠', 'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+    'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠', 'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+    'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒', 'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+    'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠', 'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+    'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋', 'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+    'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋', 'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+    'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е', 'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+    'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋', 'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+    'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠', 'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+    'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋', 'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+    'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋', 'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+    ' ': ' '
+}
+
+
+def generate_card(i):
+    selected_skills = random.sample(skills, 3)
+
+    runic_skills = []
+    for skill in selected_skills:
+        runic_skill = skill
+        for letter, rune in letters.items():
+            runic_skill = runic_skill.replace(letter, rune)
+        runic_skills.append(runic_skill)
+
+    context = {
+        "first_name": fake.first_name(),
+        "last_name": fake.last_name(),
+        "job": fake.job(),
+        "town": fake.city(),
+        "strength": random.randint(3, 18),
+        "agility": random.randint(3, 18),
+        "endurance": random.randint(3, 18),
+        "intelligence": random.randint(3, 18),
+        "luck": random.randint(3, 18),
+        "skill_1": runic_skills[0],
+        "skill_2": runic_skills[1],
+        "skill_3": runic_skills[2]
+    }
+
+    
+    output_file = os.path.join(folder_path, f"card_{i + 1}.svg")
+
+    
+    with open(output_file, "w") as my_file:  
+        render_template("template.svg", output_file, context)
+
+    print(f"Карточка {i + 1} успешно сохранена в {output_file}")
+
+
+def main():
+    
+    for i in range(10):
+        generate_card(i)
+
+
+if __name__ == '__main__':
+    main()
